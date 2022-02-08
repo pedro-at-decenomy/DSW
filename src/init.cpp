14553e0d7c5a589bd63e4b9152b868f7de78b54f
pedro-at-decenomy <sapphire_pt@sappcoin.com>
2022-02-08 01:47:18 +0000

    adds hostip option to specify the output tcp ip address or network interface

--- src/init.cpp
+++ src/init.cpp
@@ -430,6 +430,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-dnsseed", _("Query for peer addresses via DNS lookup, if low on addresses (default: 1 unless -connect/-noconnect)"));
     strUsage += HelpMessageOpt("-externalip=<ip>", _("Specify your own public address"));
     strUsage += HelpMessageOpt("-forcednsseed", strprintf(_("Always query for peer addresses via DNS lookup (default: %u)"), DEFAULT_FORCEDNSSEED));
+    strUsage += HelpMessageOpt("-hostip=<ip>", _("Specify default host IP for outbound connections"));
     strUsage += HelpMessageOpt("-listen", strprintf(_("Accept connections from outside (default: %u if no -proxy or -connect/-noconnect)"), DEFAULT_LISTEN));
     strUsage += HelpMessageOpt("-listenonion", strprintf(_("Automatically create Tor hidden service (default: %d)"), DEFAULT_LISTEN_ONION));
     strUsage += HelpMessageOpt("-maxconnections=<n>", strprintf(_("Maintain at most <n> connections to peers (default: %u)"), DEFAULT_MAX_PEER_CONNECTIONS));
@@ -1394,6 +1395,18 @@ bool AppInit2()
     // see Step 2: parameter interactions for more information about these
     fListen = GetBoolArg("-listen", DEFAULT_LISTEN);
     fDiscover = GetBoolArg("-discover", true);
+
+    if (mapArgs.count("-hostip")) {
+        std::string hostipArg = GetArg("-hostip", "");
+
+        std::string strHost;
+        int port = 0;
+        SplitHostPort(hostipArg, port, strHost);
+
+        if (!Lookup(strHost.c_str(), sHostIp, port, false)) {
+            return UIError(strprintf(_("HostIP: invalid hostip address %s\n"), hostipArg));
+        }
+    }
 
     bool fBound = false;
     if (fListen) {
